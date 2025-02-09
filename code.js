a494b8896e45a67c396b7305533fe130a16258da
nataliesleight <nsleight@uwyo.edu>
2025-02-08 20:53:40 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
 function insertionSortReverse(array) {
+    for(var i = array.length -1; i >= 0; i--) {
+        var val = array[i];
+        var j;
+        for(j = i; j < array.length && array[j+1] < val; j++) {
+            array[j] = array[j+1];
+        }
+        array[j] = val;
+    }
     return array;
 }
